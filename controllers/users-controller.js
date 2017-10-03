a25563fa0418df70ebff43f7ef9903fe55d3fef8
Mitchel Severe <truth531@gmail.com>
2017-10-03 15:11:30 -0400

    create users controller

---
+++ controllers/users-controller.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../db/config');
+const User = require('../models/user');
+
+const usersController = {};
+
+usersController.create = (req, res) => {
+  let newUser = new User({
+    username: req.body.username,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    err ? res.json({success: false, msg: 'Failed to register user'}) : res.json({success: true, msg: 'User registered'});
+  })
+}
