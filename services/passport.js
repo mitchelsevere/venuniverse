066217eee6569f4a2b578f73867ce40a6f33022b
Mitchel Severe <truth531@gmail.com>
2017-10-03 19:42:50 -0400

    create passport strategy

---
+++ services/passport.js
@@ -0,0 +1,8 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+const config = require('../db/config');
+
+module.exports = function(passport) {
+
+}
