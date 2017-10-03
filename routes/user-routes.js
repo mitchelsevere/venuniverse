b95bc0b674e4c4f0acea18935b26cf60f377d7a0
Mitchel Severe <truth531@gmail.com>
2017-10-03 14:56:10 -0400

    create user routes

---
+++ routes/user-routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const usersController = require('../controllers/users-controller');
+
+const userRouter = express.Router();
+
+userRouter.get('/profile', usersController.index);
+userRouter.post('/register', usersController.create);
+// userRouter.post('/authenticate', usersController.authenticate);
+// userRouter.post('/validate', usersController.validate);
+
+module.exports = userRouter;
