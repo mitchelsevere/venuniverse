4ed44303ea41c6b5618b597c3aad77a264805bd1
Mitchel Severe <truth531@gmail.com>
2017-10-03 20:11:43 -0400

    add authenticate method

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -5,6 +5,38 @@ const User = require('../models/user');
 
 const usersController = {};
 
+usersController.authenticate = (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if(err) throw err;
+    if (!user) return res.json({success: false, msg: 'User not found'});
+  });
+
+  User.comparePassword(password, user.password, (err, isMatch) => {
+    if(err) throw err;
+    if(isMatch) {
+      let token = jwt.sign(user, config.secret, {
+        expiresIn: 604800 // one week
+      });
+      res.json({
+        success: true,
+        token: `JWT ${token}`,
+        user: {
+          id: user._id,
+          username: user.username,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email
+        }
+      });
+    } else {
+      return res.json({success: false, msg: 'Wrong Password'});
+    }
+  });
+}
+
 usersController.create = (req, res) => {
   let newUser = new User({
     username: req.body.username,
