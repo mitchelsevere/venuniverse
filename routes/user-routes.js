e362f32846119c06a0c266e405a51ed7be860a6b
Mitchel Severe <truth531@gmail.com>
2017-10-03 22:35:39 -0400

    add passport authenticate helper to profile route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const usersController = require('../controllers/users-controller');
 
 const userRouter = express.Router();
 
-// userRouter.get('/profile', usersController.index);
+userRouter.get('/profile', passport.authenticate('jwt', { session: false }), usersController.index);
 userRouter.post('/register', usersController.create);
-// userRouter.post('/authenticate', usersController.authenticate);
+userRouter.post('/authenticate', usersController.authenticate);
 // userRouter.post('/validate', usersController.validate);
 
 module.exports = userRouter;
